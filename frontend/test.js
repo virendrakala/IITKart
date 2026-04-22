505ed70a63189474509ab4579d413e708617e393
virendrakala <virendrakala34@gmail.com>
2026-04-22 19:14:11 +0530

    chore: remove coverage, uploads, and temporary test scripts from version control

--- frontend/test.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('../Backend1/node_modules/@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const user = await prisma.user.findUnique({ where: { email: 'virendrak24@iitk.ac.in' } });
-  console.log('USER:', user);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
